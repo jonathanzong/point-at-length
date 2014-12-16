4d876851e7d86b85f3f86c72f2714f00f9361045
James Halliday <mail@substack.net>
2014-12-15 16:39:41 -0800

    somewhat more correct at function

--- index.js
+++ index.js
@@ -10,17 +10,16 @@ function Points (path) {
 }
 
 Points.prototype.at = function (pos) {
-    return this._walk(function (len) {
-        return len >= pos;
-    }).pos;
+    return this._walk(pos).pos;
 };
 
 Points.prototype.length = function () {
-    return this._walk().length;
+    return this._walk(null).length;
 };
 
-Points.prototype._walk = function (fn) {
+Points.prototype._walk = function (pos) {
     var cur = [ 0, 0 ];
+    var prev = [ 0, 0, 0 ];
     var len = 0;
     
     for (var i = 0; i < this._path.length; i++) {
@@ -30,27 +29,50 @@ Points.prototype._walk = function (fn) {
             cur[1] = p[2];
         }
         else if (p[0] === 'C') {
+            prev[0] = cur[0];
+            prev[1] = cur[1];
+            prev[2] = len;
+            
             for (var j = 0; j <= 100; j++) {
                 var t = j / 100;
-                var x = Math.pow((1-t), 3) * cur[0]
-                    + 3 * Math.pow((1-t), 2) * t * p[1]
-                    + 3 * (1-t) * Math.pow(t, 2) * p[3]
-                    + Math.pow(t, 3) * p[5]
-                ;
-                var y = Math.pow((1-t), 3) * cur[1]
-                    + 3 * Math.pow((1-t), 2) * t * p[2]
-                    + 3 * (1-t) * Math.pow(t, 2) * p[4]
-                    + Math.pow(t, 3) * p[6]
-                ;
+                var x = xof(p, t);
+                var y = yof(p, t);
                 len += dist(cur[0], cur[1], x, y);
                 
                 cur[0] = x;
                 cur[1] = y;
-                if (fn && fn(len)) return { length: len, pos: cur };
+                
+                if (typeof pos === 'number' && len >= pos) {
+                    var dv = (len - pos) / (len - prev[2]);
+                    
+                    var npos = [
+                        cur[0] * (1 - dv) + prev[0] * dv,
+                        cur[1] * (1 - dv) + prev[1] * dv
+                    ];
+                    return { length: len, pos: npos };
+                }
+                prev[0] = cur[0];
+                prev[1] = cur[1];
+                prev[2] = len;
             }
         }
     }
     return { length: len, pos: cur };
+    
+    function xof (p, t) {
+        return Math.pow((1-t), 3) * cur[0]
+            + 3 * Math.pow((1-t), 2) * t * p[1]
+            + 3 * (1-t) * Math.pow(t, 2) * p[3]
+            + Math.pow(t, 3) * p[5]
+        ;
+    }
+    function yof (p, t) {
+        return Math.pow((1-t), 3) * cur[1]
+            + 3 * Math.pow((1-t), 2) * t * p[2]
+            + 3 * (1-t) * Math.pow(t, 2) * p[4]
+            + Math.pow(t, 3) * p[6]
+        ;
+    }
 };
 
 function dist (ax, ay, bx, by) {
